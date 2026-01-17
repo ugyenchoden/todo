fe200ccf82749e6236d243878680c053f7813acb
ugyenchoden <cugyen20@gmail.com>
2026-01-17 19:31:16 +1000

    priority tag

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -3,10 +3,10 @@ interface TodoPageProps {
 }
 
 const todoItems = [
-    { id: 1, title: 'Buy groceries', dueDate: new Date().toISOString().split('T')[0], completed: false },
-    { id: 2, title: 'Walk the dog', dueDate: '2024-06-11', completed: true },
-    { id: 3, title: 'Read a book', dueDate: '2024-06-15', completed: true },
-    { id: 4, title: 'testing', dueDate: '2026-01-14', completed: false }
+    { id: 1, title: 'Buy groceries', dueDate: new Date().toISOString().split('T')[0], completed: false, priority: 'low' },
+    { id: 2, title: 'Walk the dog', dueDate: '2024-06-11', completed: true, priority: 'medium' },
+    { id: 3, title: 'Read a book', dueDate: '2024-06-15', completed: true, priority: 'high' },
+    { id: 4, title: 'testing', dueDate: '2026-01-14', completed: false, priority: 'low' }
 ];
 
 function TodoPage({ filter }: TodoPageProps) {
@@ -39,9 +39,25 @@ function TodoPage({ filter }: TodoPageProps) {
         <h1>{filter === "all" ? "Home" : filter === "today" ? "Today" : "Week"}</h1>
         <ul style={{listStyleType: 'none'}}>
             {filteredTodos.map(todo => (
-                <li key={todo.id}>
-                    <input type="checkbox" checked={todo.completed}></input>
-                    {todo.title} - Due: {todo.dueDate}
+                <li key={todo.id} 
+                className={`priority-${todo.priority}`}
+                style={{
+                     display: 'flex',
+                     alignItems: 'center',
+                     gap: '10px',
+                     padding: '12px',
+                     cursor: 'pointer',
+                     margin: '5px',
+                     width: '100vh'
+                }}
+                onMouseEnter={(e) => (
+                    e.currentTarget.style.backgroundColor = '#333', 
+                    e.currentTarget.style.boxShadow = 'rgba(0, 0, 0, 0.2) 3px 3px 5px 2px'
+                )}
+                onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = 'transparent')}
+                >
+                     <input type="checkbox" checked={todo.completed}></input>
+                        {todo.title} - Due: {todo.dueDate}
                 </li>
             ))}
         </ul>
