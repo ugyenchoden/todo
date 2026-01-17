02e043e0e8a69c47d9389105f2aa76978b74adb6
ugyenchoden <cugyen20@gmail.com>
2026-01-17 20:06:45 +1000

    button

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -3,14 +3,13 @@ interface TodoPageProps {
 }
 
 const todoItems = [
-    { id: 1, title: 'Buy groceries', dueDate: new Date().toISOString().split('T')[0], completed: false, priority: 'low' },
-    { id: 2, title: 'Walk the dog', dueDate: '2024-06-11', completed: true, priority: 'medium' },
-    { id: 3, title: 'Read a book', dueDate: '2024-06-15', completed: true, priority: 'high' },
-    { id: 4, title: 'testing', dueDate: '2026-01-14', completed: false, priority: 'low' }
+    { id: 1, title: 'Buy groceries', dueDate: new Date().toISOString().split('T')[0], completed: false, priority: 'low', category: 'general' },
+    { id: 2, title: 'Walk the dog', dueDate: '2024-06-11', completed: true, priority: 'medium', category: 'general' },
+    { id: 3, title: 'Read a book', dueDate: '2024-06-15', completed: true, priority: 'high', category: 'projects' },
+    { id: 4, title: 'testing', dueDate: '2026-01-14', completed: false, priority: 'low', category: 'projects' }
 ];
 
 function TodoPage({ filter }: TodoPageProps) {
-
     const filteredTodos = todoItems.filter(todo => {
     const today = new Date();
     const dueDate = new Date(todo.dueDate);
@@ -36,20 +35,11 @@ function TodoPage({ filter }: TodoPageProps) {
 
     return(
     <>
-        <h1>{filter === "all" ? "Home" : filter === "today" ? "Today" : "Week"}</h1>
+        <h1 style={{paddingLeft: '40px'}}>{filter === "all" ? "Home" : filter === "today" ? "Today" : "Week"}</h1>
         <ul style={{listStyleType: 'none'}}>
             {filteredTodos.map(todo => (
                 <li key={todo.id} 
-                className={`priority-${todo.priority} ${todo.completed? 'text-strike': null }`}
-                style={{
-                     display: 'flex',
-                     alignItems: 'center',
-                     gap: '10px',
-                     padding: '12px',
-                     cursor: 'pointer',
-                     margin: '5px',
-                     width: '100vh'
-                }}
+                className={`priority-${todo.priority}  todo-item`}
                 onMouseEnter={(e) => (
                     e.currentTarget.style.backgroundColor = '#333', 
                     e.currentTarget.style.boxShadow = 'rgba(0, 0, 0, 0.2) 3px 3px 5px 2px'
@@ -57,7 +47,10 @@ function TodoPage({ filter }: TodoPageProps) {
                 onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = 'transparent')}
                 >
                      <input type="checkbox" checked={todo.completed}></input>
-                     {todo.title} - Due: {todo.dueDate}
+                     <span className={`${todo.completed? 'text-strike': null }`}> {todo.title} - Due: {todo.dueDate}</span>
+                    
+                     <button style={{alignItems: 'end'}} type="submit">Edit</button>
+                     <button style={{alignItems: 'end'}} type="submit">Delete</button>
                 </li>
             ))}
         </ul>
